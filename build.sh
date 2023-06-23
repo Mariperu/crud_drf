fe945a8b976aa882e7ebf8a9543ec2cfc00b09e4
Maritza Rodriguez <maritzarg2020@gmail.com>
2023-06-23 11:53:25 -0500

    createsuperuser3

--- build.sh
+++ build.sh
@@ -6,4 +6,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python drfcrud_test/manage.py createsuperuser --no-input
+fi
